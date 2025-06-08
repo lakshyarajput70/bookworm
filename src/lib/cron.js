933a2223441b3bebe94b1fe0d274fef5cae6335d
lakshyarajput70 <lakshyarajput70@gmail.com>
2025-06-08 19:33:15 +0530

    cloudinary error resolving 2

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -4,7 +4,7 @@ import https from 'https';
 const job = new cron.CronJob("*/14 * * * *",function(){
     https.get(process.env.API_URL,(res)=>{
         if(res.statusCode === 200) console.log("GET request sent successfully");
-        else console.log("GET requset failde",res.statusCode);
+        else console.log("GET requset failed",res.statusCode);
     })
     .on("error",(e)=>console.error("Error while sending request",e));
 });
